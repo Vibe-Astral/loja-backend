599eb40158eb1ad745f522a64ab70ac8ab12cba2
Vibe-Astral <jorgeantonio.acll@gmail.com>
2025-10-27 19:53:29 -0300

    Correção rota estoque dentro do modulo movimentação

--- src/movimentacao/movimentacao.module.ts
+++ src/movimentacao/movimentacao.module.ts
@@ -3,11 +3,10 @@ import { MovimentacaoService } from './movimentacao.service';
 import { MovimentacaoController } from './movimentacao.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrismaModule } from '../prisma/prisma.module';
-import { EstoqueController } from '../estoque/estoque.controller'; // se você colocou lá
 
 @Module({
   imports: [PrismaModule],
-  controllers: [MovimentacaoController, EstoqueController],
+  controllers: [MovimentacaoController],
   providers: [MovimentacaoService, PrismaService],
   exports: [MovimentacaoService],
 })
